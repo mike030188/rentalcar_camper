3e80ce369306c39c34223d09438e0e6fd2910767
mike030188 <halol.imnida2023@gmail.com>
2023-08-05 10:22:20 +0500

    feat: Navbar is completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,55 @@
-import { NavbarWrapper } from "./Navbar.style";
+import { MenuItems, NavIcons, NavMenu, NavbarWrapper, Text } from "./Navbar.style";
+import {FaCartPlus, FaUser} from 'react-icons/fa';
 
 export default function NavbarComp() { 
     return(
         <NavbarWrapper>
-            
+            <Text>Camper</Text>
+            <NavMenu>
+                <MenuItems>
+                    <select name="cars" id="cars">
+                        <option value="motor">Motor</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </MenuItems>
+                <MenuItems>
+                    <select name="cars" id="cars">
+                        <option value="motor">Caravan</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </MenuItems>
+                <MenuItems>
+                    <select name="cars" id="cars">
+                        <option value="motor">Tuning</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </MenuItems>
+                <MenuItems>
+                    <select name="cars" id="cars">
+                        <option value="motor">Used car</option>
+                        <option value="saab">Saab</option>
+                        <option value="opel">Opel</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </MenuItems>
+                <MenuItems><span>Camping Place</span></MenuItems>
+            </NavMenu>
+            <NavIcons>
+                <FaCartPlus style={{color:'#006DAB'}}/>
+                <FaUser style={{marginLeft:'25px', color:'#006DAB'}}/>
+                <select name="language" id="language" style={{border:'none', marginLeft:'25px'}}>
+                        <option value="english">En</option>
+                        <option value="russian">Rus</option>
+                        <option value="uzbek">Uz</option>
+                        <option value="korean">Kor</option>
+                    </select>
+            </NavIcons>
         </NavbarWrapper>
     )
 }
--- src/components/navbar/Navbar.style.js
+++ src/components/navbar/Navbar.style.js
@@ -1,8 +1,48 @@
 import styled from 'styled-components';
 
 export const NavbarWrapper = styled.div`
+display: flex;
+align-items: center;
 width: 100%;
 height: 90px;
+padding: 0 70px;
 background: #FFF;
+font-weight: 600;
 box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.10);
+`
+
+export const Text = styled.div`
+flex: 1;
+color: var(--blue, #006DAB);
+font-size: 38px;
+line-height: normal;
+`
+export const NavMenu = styled.div`
+display: flex;
+flex: 3;
+justify-content: space-around;
+
+
+`
+
+export const MenuItems = styled.div`
+    
+select{
+    color: var(--text, #373737);
+    font-size: 16px;
+    line-height: normal;
+    border: none;
+}
+
+span{
+    color: var(--text, #373737);
+    font-size: 16px;
+    line-height: normal;
+}
+`
+
+export const NavIcons = styled.div`
+display: flex;
+flex:1;
+justify-content: right;
 `
