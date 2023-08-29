ff0d42257f735f7b862f1b895f1aa003eee6fa8d
mike030188 <halol.imnida2023@gmail.com>
2023-08-29 20:14:26 +0500

    feat: SignIn page is completed

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
-import { Container, SignInWrapper } from '../styles/SignIn.style';
+import { Checkbox, Container, Divider1, FormControl, Icons, SignInWrapper } from '../styles/SignIn.style';
+import { Button } from '../styles/CarInfo.style';
+import GoogleIcon from '@mui/icons-material/Google';
+import { SiNaver } from 'react-icons/si';
+import { RiKakaoTalkFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const SignInComp = () => {
   return (
     <Container>
         <SignInWrapper>
-
+            <FormControl>
+                <h1>Sign in</h1>
+                <label for='email'>Email</label>
+                <input type="email" placeholder='Your email'/>
+                <label for='password'>Password</label>
+                <input type="password" placeholder='Your password'/>
+            </FormControl>
+            <Checkbox>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <input type="checkbox" />
+                    <label for="">Keep me logged in</label>
+                </div>
+                <div>
+                    <a href="/registrate">Forget your password ?</a>
+                </div>
+            </Checkbox>
+            
+            <Button add wd='100%' hgt='45px' style={{marginTop: "27px"}}>SIGN IN</Button>
+            <Divider1 class="divider">OR</Divider1>
+            <Icons>
+                <Link to='https://www.naver.com/index.html' style={{textDecoration: "none"}}>
+                    <div style={{background:'#039600', color:'#FFF'}}>
+                        <SiNaver/>
+                    </div>
+                </Link>
+                <Link to='https://www.google.com/index.html' style={{textDecoration: "none"}}>
+                    <div style={{background:'#BC0000', color:'#FFF', margin:'0 11px'}}>
+                        <GoogleIcon/>
+                    </div>
+                </Link>
+                <Link to='https://www.kakaocorp.com/page/service/service/KakaoTalk?lang=ENG&tab=all' style={{textDecoration: "none"}}>
+                    <div style={{background:'#FFC700', color:'black', fontSize:'32px'}}>
+                        <RiKakaoTalkFill/>
+                    </div>
+                </Link>
+            </Icons>
+            <Link to='/registrate' style={{textDecoration: "none"}}>
+                <Button wd='100%' hgt='45px' br='10px' bg='transparent' style={{border: '1px solid #006DAB', color:'#006DAB'}}>CREATE ACCOUNT</Button>
+            </Link>
         </SignInWrapper>
     </Container>
   )
--- src/components/styles/SignIn.style.js
+++ src/components/styles/SignIn.style.js
@@ -10,9 +10,115 @@ background-color: #FAFAFA;
 `
 
 export const SignInWrapper = styled.div`
+padding: 33px 33px 49px 33px;
 width: 500px;
 height: 614px;
 border-radius: 20px;
 background: #FFF;
 box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.10);
+`
+
+export const FormControl = styled.div`
+display: flex;
+flex-direction: column;
+font-family: 'Open Sans', sans-serif;
+line-height: normal;
+
+h1{
+    color: var(--text, #373737);
+    font-size: 27px;
+    font-weight: 700;
+    line-height: normal;
+    margin-bottom: 25PX;
+}
+
+label{
+    color: rgba(55, 55, 55, 0.80);
+    font-size: 16px;
+    font-weight: 600;
+    line-height: normal;
+}
+
+input{
+    width: 100%;
+    height: 50px;
+    color: rgba(55, 55, 55, 0.60);
+    font-size: 15px;
+    font-weight: 400;
+    border-radius: 10px;
+    background: rgba(55, 55, 55, 0.10);
+    border: none;
+    padding-left: 20px;
+    margin: 10px 0 19px 0;
+}
+`
+
+export const Checkbox = styled.div`
+display: flex;
+justify-content:space-between;
+align-items:center;
+font-family: Open Sans;
+line-height: normal;
+color: #373737;
+
+input{
+    width: 23px;
+    height: 22px;
+    border-radius: 5px;
+    background: rgba(55, 55, 55, 0.10);
+}
+
+label{
+    font-size: 14px;
+    font-weight: 400;
+    margin-left: 7px;
+}
+
+a{
+    font-size: 14px;
+    font-weight: 400;
+    text-decoration: none;
+    cursor: pointer
+}
+`
+
+export const Divider1 = styled.div`
+margin-top: 20px;
+
+& {
+    color: #6D6D6D;
+    font-family: Open Sans;
+    font-size: 14px;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+}
+
+&:before, &:after {
+  flex: 1;
+  content: '';
+  padding: 1px;
+  background-color: rgba(55, 55, 55, 0.15);
+  margin: 10px 25px;
+}
+`
+export const Icons = styled.div`
+display: flex;
+justify-content: center;
+margin:20px 0;
+
+div{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 46px;
+    height: 46px;
+    border-radius: 50%;
+    cursor: pointer;
+
+    &:hover{
+        opacity: 0.9;
+        transform: scale(0.98);
+    }
+}
 `
